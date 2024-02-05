465e16bb3c9ae62d67b5758927a6968d6843f83a
abel abraha <abrahaabel18@gmail.com>
2024-02-05 14:54:36 +0000

    post-function

--- server/server.js
+++ server/server.js
@@ -9,3 +9,33 @@ const db = Database("database.db");
 app.listen("0001", () => {
   console.log("port is working");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+app.post (`/memories`, (req, res) =>{
+  try{
+    const username = req.body.username
+    const message = req.body.message
+    const date = req.body.date
+    const location = req.body.location
+    const picture = req.body.picture 
+
+    const newMemories = db.prepare(`INSERT INTO memories (username, message, date, location, picture) VALUES(?,?,?,?,?)`).run
+    (username,message,date,location,picture)
+    res.status(200).json(newMemories)
+  } catch(err) {
+    res.status(500).json({error : err})
+  }
+
+})
