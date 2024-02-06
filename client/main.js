3f79f3a36cf0228f19e3744f2f854324c4e3b629
Max Emerson <maxemerson2013@gmail.com>
2024-02-06 09:38:48 +0000

    added Event listener

--- client/main.js
+++ client/main.js
@@ -1,1 +1,21 @@
 const baseURL = import.meta.env.VITE_Server;
+const form = document.getElementById("memoryLog");
+const existingData = document.getElementById("existingMemories");
+
+form.addEventListener("sumit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const memoryData = Object.fromEntries(formData);
+  const response = await fetch(`${baseURL}/memories`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(memoryData),
+  });
+  if (response.ok) {
+    displayMemories();
+  } else {
+    console.log("failed to add", response.status);
+  }
+});
